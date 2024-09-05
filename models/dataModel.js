10d97b7afed0ff9223d033832bfc04976d25759b
Sad4k <thsmall99@gmail.com>
2024-09-05 01:25:19 -0400

    auth

--- models/dataModel.js
+++ models/dataModel.js
@@ -1,19 +1,93 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../server').sequelize;
 
+// Modelo de Usuario
 const User = sequelize.define('User', {
   username: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   email: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   password: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  plan: {
+    type: DataTypes.ENUM('free', 'basic', 'premium'),
+    defaultValue: 'free'
   }
 });
 
-module.exports = User;
+// Modelo de Aplicación
+const App = sequelize.define('App', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  }
+});
+
+// Modelo de Licencia
+const License = sequelize.define('License', {
+  licenseKey: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  expiryDate: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+});
+
+// Modelo de Instalación (para relacionar usuarios con aplicaciones)
+const Installation = sequelize.define('Installation', {
+  installedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+});
+
+// Relaciones
+// Un Usuario puede tener muchas Instalaciones
+User.hasMany(Installation);
+Installation.belongsTo(User);
+
+// Una Aplicación puede ser instalada por muchos Usuarios
+App.hasMany(Installation);
+Installation.belongsTo(App);
+
+// Un Usuario puede tener muchas Licencias
+User.hasMany(License);
+License.belongsTo(User);
+
+// Una Aplicación puede estar asociada a muchas Licencias
+App.hasMany(License);
+License.belongsTo(App);
+
+module.exports = {
+  User,
+  App,
+  License,
+  Installation
+};
